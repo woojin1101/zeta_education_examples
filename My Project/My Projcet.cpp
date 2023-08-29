0c30f889851da9cf4e781403b59cadedd7bffd8b
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-08-29 15:52:21 +0900

    Update My Projcet.cpp

--- My Project/My Projcet.cpp
+++ My Project/My Projcet.cpp
@@ -4,18 +4,10 @@ using namespace std;
 int main()
 
 {
-
     int age;
-
-
     cout << "여러분의 나이를 입력해 주세요 : ";
-
-    cin >> age;
-
- 
-
+    cin >> age; 
     cout << "여러분의 나이는 " << age << "살 입니다." << endl;
-
     return 0;
 
 }
