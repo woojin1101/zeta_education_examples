0b49e4d45d942bd89a540d0af3d7c1c9f68280b9
apro2656 <apro2656@gmail.com>
2023-08-30 18:02:06 +0900

    2023.08.30

---
+++ my_python_example/memo.py
@@ -0,0 +1,26 @@
+# write or read, erase memo.txt file.
+
+import sys
+
+option = sys.argv[1]
+
+if option == '-w':
+    memo = sys.argv[2]
+    f = open('memo.txt', 'a')
+    f.write(memo)
+    f.write('\n')
+    f.close()
+
+elif option == '-r':
+    f = open('memo.txt')
+    memo = f.read()
+    f.close()
+    print(memo)
+
+elif option == '-e':
+    with open('memo.txt', 'w') as f:
+        f.truncate(0)
+    print("All text has been erased!")
+
+else:
+    print("There is no correct argument!")
