bc3592abd28e78921dd67978884a473ebb3f9bcb
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-09-03 17:11:53 +0900

    Update server_class.cpp

--- C++_CLASS/server_class.cpp
+++ C++_CLASS/server_class.cpp
@@ -40,41 +40,6 @@ int main() {
 
     return 0;
 }
-수정된 클라이언트 코드 (client.cpp)
-cpp
-Copy code
-#include <iostream>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-
-int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        std::cerr << "Failed to create socket\n";
-        return -1;
-    }
-
-    sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection failed\n";
-        return -1;
-    }
-
-    send(sock, "Hello from client", 17, 0);
-
-    char buffer[1024] = {0};
-    read(sock, buffer, 1024);
-    std::cout << "Message received: " << buffer << std::endl;
-
-    return 0;
-}
-
-
 
 
 
