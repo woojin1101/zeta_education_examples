c7943c7ad010e29a145ec5ef6686bed144587868
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-09-03 17:26:06 +0900

    Update client_class.cpp

--- C++_CLASS/client_class.cpp
+++ C++_CLASS/client_class.cpp
@@ -12,7 +12,7 @@ int main() {
 
     sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(6080);
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
