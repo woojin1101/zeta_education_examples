98c2f6aa8e8e0db69f314305b251e3f82d3c937c
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-09-03 17:11:01 +0900

    Update server_class.cpp

--- C++_CLASS/server_class.cpp
+++ C++_CLASS/server_class.cpp
@@ -3,51 +3,79 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-class Server {
-public:
-    Server() {
-        server_fd = socket(AF_INET, SOCK_STREAM, 0);
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(8080);
-    }
-
-    bool bindSocket() {
-        return (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) >= 0);
-    }
-
-    bool listenSocket() {
-        return (listen(server_fd, 3) >= 0);
-    }
-
-    int acceptClient() {
-        int addrlen = sizeof(address);
-        return accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-    }
-
-private:
-    int server_fd;
-    struct sockaddr_in address;
-};
-
 int main() {
-    Server server;
-
-    if (!server.bindSocket() || !server.listenSocket()) {
-        std::cerr << "Socket operation failed.\n";
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket\n";
         return -1;
     }
 
-    int new_socket = server.acceptClient();
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed\n";
+        return -1;
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed\n";
+        return -1;
+    }
+
+    int addrlen = sizeof(address);
+    int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
     if (new_socket < 0) {
-        std::cerr << "Accept failed.\n";
+        std::cerr << "Accept failed\n";
         return -1;
     }
 
     char buffer[1024] = {0};
     read(new_socket, buffer, 1024);
     std::cout << "Message received: " << buffer << std::endl;
-
     send(new_socket, "Hello from server", 17, 0);
+
     return 0;
 }
+수정된 클라이언트 코드 (client.cpp)
+cpp
+Copy code
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        std::cerr << "Failed to create socket\n";
+        return -1;
+    }
+
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection failed\n";
+        return -1;
+    }
+
+    send(sock, "Hello from client", 17, 0);
+
+    char buffer[1024] = {0};
+    read(sock, buffer, 1024);
+    std::cout << "Message received: " << buffer << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
