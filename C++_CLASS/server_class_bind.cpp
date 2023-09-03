45ff9bd0a67e32fd26738499928c212c00dc7e0d
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-09-03 17:45:30 +0900

    Update server_class_bind.cpp

--- C++_CLASS/server_class_bind.cpp
+++ C++_CLASS/server_class_bind.cpp
@@ -4,16 +4,23 @@
 #include <unistd.h>
 
 int main() {
+    int opt = 1;
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         std::cerr << "Failed to create socket\n";
         return -1;
     }
 
+    // SO_REUSEADDR 옵션 설정
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        std::cerr << "setsockopt failed\n";
+        return -1;
+    }
+
     sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(6080);
+    address.sin_port = htons(6000);  // 또는 다른 사용하지 않는 포트 번호
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         std::cerr << "Bind failed\n";
