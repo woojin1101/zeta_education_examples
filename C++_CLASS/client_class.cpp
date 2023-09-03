f531c90ca81d047abaa51ed58fe5ff1fbacf4cc8
ZetaBank <67315518+ZetaBank@users.noreply.github.com>
2023-09-03 17:12:12 +0900

    Update client_class.cpp

--- C++_CLASS/client_class.cpp
+++ C++_CLASS/client_class.cpp
@@ -3,40 +3,27 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-class Client {
-public:
-    Client() {
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(8080);
-    }
-
-    bool connectToServer() {
-        inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-        return (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) >= 0);
-    }
-
-    int getSock() {
-        return sock;
-    }
-
-private:
-    int sock;
-    struct sockaddr_in serv_addr;
-};
-
 int main() {
-    Client client;
-
-    if (!client.connectToServer()) {
-        std::cerr << "Connection failed.\n";
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        std::cerr << "Failed to create socket\n";
         return -1;
     }
 
-    send(client.getSock(), "Hello from client", 17, 0);
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection failed\n";
+        return -1;
+    }
+
+    send(sock, "Hello from client", 17, 0);
 
     char buffer[1024] = {0};
-    read(client.getSock(), buffer, 1024);
+    read(sock, buffer, 1024);
     std::cout << "Message received: " << buffer << std::endl;
 
     return 0;
